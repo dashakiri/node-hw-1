2e57cf4e58165737a816e3672e9c30aecfa6d963
Dasha Kirienko <dkirienko25@gmail.com>
2021-11-30 11:25:53 +0300

    switched parseInt to String

--- contacts/getById.js
+++ contacts/getById.js
@@ -2,7 +2,7 @@ const getAllContacts = require("./getAllContacts");
 
 const getById = async(id) => {
     const contacts = await getAllContacts();
-    const filteredContact = contacts.find(contact => parseInt(contact.id) === parseInt(id));
+    const filteredContact = contacts.find(contact => String(contact.id) === String(id));
     if(!filteredContact) {
         return null;
     };
--- contacts/removeContact.js
+++ contacts/removeContact.js
@@ -3,7 +3,7 @@ const updateContacts = require("./updateContacts");
 
 const removeById = async(id) => {
     const contacts = await getAllContacts();
-    const index = contacts.findIndex(contact => parseInt(contact.id) === parseInt(id));
+    const index = contacts.findIndex(contact => String(contact.id) === String(id));
     if(index === -1) {
         return null;
     };
